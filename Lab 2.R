ade824a38eb06b8a292ebce94948508c52f28aa4
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 21:53:53 +0530

    Update Lab 2.R

--- Lab 2.R
+++ Lab 2.R
@@ -24,3 +24,38 @@ if(x>0){
 }else{
   print("X is a 0")
 }
+
+x <- 10
+if (x < 0) {
+  print("Negative")
+} else if (x == 0) {
+  print("Zero")
+} else if (x <= 10) {
+  print("Between 1 and 10")
+} else {
+  print("Greater than 10")
+}
+
+# while loop
+x <- 1
+while (x <= 5) {
+  print(x)
+  x <- x + 1
+}
+
+# for loop
+for (i in 1:10) {
+  print(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
