d7d6b94829e9db9001744d8ac736399254c725ab
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 21:51:05 +0530

    Update Lab 2.R

--- Lab 2.R
+++ Lab 2.R
@@ -1,1 +1,26 @@
-setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Labsheets")
+setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Lab 02-20250502")
+getwd()
+
+### Control Statement
+
+# if 
+
+x<--4
+x
+
+if(x>0){
+  print("X is a positive number")
+}else{
+  print("X is a negative number")
+}
+
+# nested if 
+x = 0
+
+if(x>0){
+  print("X is a positive number")
+}else if(x<0){
+  print("X is a Negative number")
+}else{
+  print("X is a 0")
+}
