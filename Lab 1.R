97528499dd60995ab5bfd08592d9542207df9c84
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 15:30:01 +0530

    Create Lab 1.R

---
+++ Lab 1.R
@@ -0,0 +1,48 @@
+# print(100:150)
+# print(-100:1)
+# comment
+
+# get current working directory 
+# getwd()
+
+# set working directory 
+# setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Labsheets")
+
+# get help
+# ?function/obj name
+# help("data.frame")
+
+print(10+1)
+
+# install packages
+# install.packages("ggplot2")
+
+# power
+4^2
+8**2
+
+# Modulus (mod)   
+4%%3
+
+# Integer Division 
+12%/%2 
+
+
+# logical operators
+x = 5
+y = 4
+
+isTRUE(x<=y)
+isTRUE(x==y)
+isTRUE(x>=y)
+
+
+
+
+
+
+
+
+
+
+
