f032a10d3ea75cb93aa4642098e899f4df0006d4
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 23:03:48 +0530

    Update Lab 2 p2.R

--- Lab 2 p2.R
+++ Lab 2 p2.R
@@ -1,4 +1,14 @@
 setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Lab 02-20250502")
 getwd()
 
-DATA 2.csv
+#### csv and text file
+# import csv file
+data1<-read.csv("DATA 2.csv")
+
+# data editor mode
+fix("data1")
+data1
+
+data2<-read.table("DATA1.txt", header=TRUE, sep = ",")
+data2
+fix("data2")
