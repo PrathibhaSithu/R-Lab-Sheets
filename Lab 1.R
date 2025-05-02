18b8ed471db91b5870a6eb72287e42ed10789325
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 15:48:16 +0530

    Update Lab 1.R

--- Lab 1.R
+++ Lab 1.R
@@ -1,21 +1,21 @@
-# print(100:150)
-# print(-100:1)
+print(100:150)
+print(-100:1)
 # comment
 
 # get current working directory 
-# getwd()
+getwd()
 
 # set working directory 
-# setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Labsheets")
+setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Labsheets")
 
 # get help
 # ?function/obj name
-# help("data.frame")
+help("data.frame")
 
 print(10+1)
 
 # install packages
-# install.packages("ggplot2")
+#install.packages("ggplot2")
 
 # power
 4^2
@@ -35,6 +35,35 @@ y = 4
 isTRUE(x<=y)
 isTRUE(x==y)
 isTRUE(x>=y)
+
+
+
+# vector
+w<-c(1,2,3,4,5,6,7)
+w
+class(w)
+
+x<-c('a','b','c','d','e','f')
+x
+class(x)
+
+z<-c(1.1,2.4,5.8,45.3,6.8)
+z
+class(z)
+
+# factor
+gender<-c(0,1,1,1,0,0,0,0,0,1,1,1,0,1,0,1,1,1,0)
+gender
+
+Gender<-factor(gender,c(0,1),c("Male","Female"))
+Gender
+class(Gender)
+
+
+
+
+
+
 
 
 
