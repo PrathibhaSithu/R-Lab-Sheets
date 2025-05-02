c2f354730e6544e50cdcb6b3138d7a0331b9c704
PrathiSithu <sithuminiprathiba@gmail.com>
2025-05-02 18:40:15 +0530

    Update Lab 1.R

--- Lab 1.R
+++ Lab 1.R
@@ -11,6 +11,9 @@ setwd("E:\\SLIIT\\4th Year\\2nd Semester\\Repeat\\PS\\Labs\\Labsheets")
 # get help
 # ?function/obj name
 help("data.frame")
+?mean       # opens help for the mean() function
+help(mean)  # same thing
+
 
 print(10+1)
 
@@ -31,11 +34,20 @@ print(10+1)
 # logical operators
 x = 5
 y = 4
+w <- 10
+z <<- 20  # used inside functions (global scope)
+
 
 isTRUE(x<=y)
 isTRUE(x==y)
 isTRUE(x>=y)
 
+
+5 > 3      # TRUE
+5 == 5     # TRUE
+!(5 > 3)   # FALSE
+TRUE & FALSE  # FALSE
+TRUE | FALSE  # TRUE
 
 
 # vector
@@ -46,6 +58,12 @@ class(w)
 x<-c('a','b','c','d','e','f')
 x
 class(x)
+
+v <- c(1, 2, 3, 4)
+names(v) <- c("A", "B", "C", "D")  # naming elements
+v[2]       # Access 2nd element
+v[v > 2]   # Subsetting
+
 
 z<-c(1.1,2.4,5.8,45.3,6.8)
 z
@@ -59,6 +77,16 @@ Gender<-factor(gender,c(0,1),c("Male","Female"))
 Gender
 class(Gender)
 
+gender2 <- factor(c("Male", "Female", "Female", "Male"))
+levels(gender2)   # Shows: "Female" "Male"
+
+# List
+mylist <- list(name="John", age=25, scores=c(85, 90))
+mylist$name   # Access "name"
+mylist$scores
+
+m <- matrix(1:6, nrow=2, ncol=3)
+m[1,2]  # Access element in 1st row, 2nd column
 
 
 
